c30a3a7fb4d222d73739342256b2b07f21e0c4bd
Your Name <you@example.com>
2022-06-27 12:14:40 +0800

    0627up

--- myconfig.py
+++ myconfig.py
@@ -52,7 +52,7 @@
 # #STEERING
 # STEERING_CHANNEL = 1            #channel on the 9685 pwm board 0-15
 STEERING_LEFT_PWM = 450         #pwm value for full left steering
-STEERING_RIGHT_PWM = 280        #pwm value for full right steering
+STEERING_RIGHT_PWM = 270        #pwm value for full right steering
 # 
 # #STEERING FOR PIGPIO_PWM
 # STEERING_PWM_PIN = 13           #Pin numbering according to Broadcom numbers
